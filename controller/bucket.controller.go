c4d52fd19e34e75c0b6c1416095650db7a5369e2
sandeep kumar <sandeepkumar@192.168.1.7>
2021-12-10 12:23:39 +0530

    feat(create bucket): added a create bucket controller

--- controller/bucket.controller.go
+++ controller/bucket.controller.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CraeteBucket(ctx *gin.Context) {
+	res, status := services.CreateBucket(ctx)
+	if !res.Success {
+		ctx.JSON(status, res)
+		return
+	}
+
+	ctx.JSON(status, res)
+}
+
 func UploadItem(ctx *gin.Context) {
 	res, status := services.UploadItem(ctx)
 	if !res.Success {
--- services/bucket.services.go
+++ services/bucket.services.go
@@ -3,10 +3,23 @@ package services
 import (
 	"awsCloud/repositry"
 	"awsCloud/utils"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
+
+func CreateBucket(ctx *gin.Context) (res Response, status int) {
+	username := ctx.Request.Header.Get("username")
+	userId := ctx.Request.Header.Get("userId")
+
+	bucketName := utils.CreateBucketName(username, userId)
+	// createing new bucket
+	_, BucketErr := repositry.CreateBucket(bucketName)
+	if BucketErr != nil {
+		return Response{Success: false, Message: BucketErr.Error(), Data: nil}, 400
+	}
+
+	return Response{Success: true, Message: "Bucket created successfully!", Data: nil}, 200
+}
 
 func UploadItem(ctx *gin.Context) (res Response, status int) {
 	username := ctx.Request.Header.Get("username")
@@ -39,6 +52,9 @@ func GetAllItem(ctx *gin.Context) (res Response, status int) {
 		return Response{Success: false, Message: err.Error(), Data: nil}, 400
 	}
 
-	fmt.Println(resp)
-	return Response{Success: true, Message: "Successfully fetched", Data: resp}, 200
+	if resp.Contents == nil {
+		return Response{Success: true, Message: "Bucket is empty!", Data: resp.Contents}, 200
+	}
+
+	return Response{Success: true, Message: "Successfully fetched", Data: resp.Contents}, 200
 }
