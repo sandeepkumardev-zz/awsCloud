c7257c791c4ee246015e11e517624578176bca0f
sandeep kumar <sandeepkumar@192.168.1.2>
2021-12-23 16:21:45 +0530

    feat: added verify OTP route

--- http/routes/user.routes.go
+++ http/routes/user.routes.go
@@ -10,4 +10,5 @@ func userRoutes(router *gin.RouterGroup) {
 	router.POST("signin", ctrl.SignIn)
 	router.POST("signup", ctrl.SignUp)
 	router.POST("refreshtoken", ctrl.RefreshToken)
+	router.POST("verifyotp", ctrl.VerifyOTP)
 }
